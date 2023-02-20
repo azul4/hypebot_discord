e6db28560a2d9047964c7c9fab411dfab6ab72f8
kjy <azul@kakao.com>
2023-02-20 23:48:15 +0900

    0220

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
-var express = require('express');
-var port = process.env.PORT || 3000;
-var app = express();
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 
-app.get('/', function(req, res) {
-  res.send({
-    "Output" : "Hello World!"
-  });
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+console.log("hello")
+
+
+// Slash Command 추가
+registerCommands(process.env.DISCORD_BOT_TOKEN, process.env.CLIENT_ID, process.env.TO_REGISTER_GUILD);
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
 });
 
-app.post('/', function(req, res) {
-  res.send({
-    "Output" : "Hello World!"
-  });
+// 메시지를 받으면 호출되는 함수
+client.on('interactionCreate', async interaction => {
+  // Original: https://discordjs.guide/interactions/replying-to-slash-commands.html#receiving-interactions
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === '안녕하세요') {
+      await interaction.reply('인사 잘한다~');
+  }
 });
 
-app.listen(port);
-module.exports = app;
+client.login(token);
