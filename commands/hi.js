47a0945e0a4ff953db1b4f1730e6967971abfeba
kjy <azul@kakao.com>
2023-02-22 00:52:20 +0900

    Slash Command Implementation

---
+++ commands/hi.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require('discord.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('안녕하세요')
+        .setDescription('해당 명령어를 실행해보세요!ㅋㅋ'),
+
+    async execute(interaction) {
+        await interaction.reply('인사잘한다')
+    }
+}
--- server.js
+++ server.js
@@ -1,14 +1,51 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const fs = require('node:fs');
+const path = require('node:path');
+const { REST, Routes, Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+const commands = [];
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command);
+    commands.push(command.data.toJSON());
+  } else {
+    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+  }
+}
 console.log("hello")
+const rest = new REST( {version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+//즉시실행 함수
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.TO_REGISTER_GUILD),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
 
 
 // Slash Command 추가
-//registerCommands(process.env.DISCORD_BOT_TOKEN, process.env.CLIENT_ID, process.env.TO_REGISTER_GUILD);
+//registerCommands(process.env.DISCORD_TOKEN, process.env.CLIENT_ID, process.env.TO_REGISTER_GUILD);
 
 
 client.on('ready', () => {
@@ -17,11 +54,17 @@ client.on('ready', () => {
 
 // 메시지를 받으면 호출되는 함수
 client.on('interactionCreate', async interaction => {
-  // Original: https://discordjs.guide/interactions/replying-to-slash-commands.html#receiving-interactions
-  if (!interaction.isCommand()) return;
+  if (!interaction.isChatInputCommand()) return;
 
-  if (interaction.commandName === '안녕하세요') {
-      await interaction.reply('인사 잘한다~');
+  //./commands에서 커맨드 찾아서 저장
+  const command = client.commands.get(interaction.commandName);
+
+  try {
+    await command.execute(interaction);
+  } catch(e) {
+    await interaction.reply( {
+      content: 'There was an error',
+    })
   }
 });
 
