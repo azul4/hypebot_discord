905725ad4a4213d95277525e060d023a2f3a65ad
azul <azul@kakao.com>
2023-03-04 16:00:30 +0900

    pm2 ecosystem added

--- commands/hi.js
+++ commands/hi.js
@@ -3,9 +3,10 @@ import { SlashCommandBuilder } from 'discord.js';
 export const HiCommand = {
     data: new SlashCommandBuilder()
         .setName('안녕하세요')
-        .setDescription('해당 명령어를 실행해보세요!ㅋㅋ'),
+        .setDescription('다시 인사해 줄 거에요.'),
 
     async execute(interaction) {
         await interaction.reply('인사잘한다')
     }
 }
+
---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+//pm2 ecosystem config 설정파일
+module.exports = {
+    apps: [{
+    name: 'server',
+    script: './server.js',
+    instances: 0,
+    exec_mode: ‘cluster’
+    }]
+  }
